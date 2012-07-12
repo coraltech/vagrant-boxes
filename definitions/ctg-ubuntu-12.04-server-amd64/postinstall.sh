2a9ff007daa47110036eef656487e332ebb48fba
Adrian Webb <adrian.webb@venturiscm.com>
2012-07-12 04:54:04 -0400

    Moving VeeWee configurations into defnitions directory and trimming the postinstall.sh.

--- definitions/ctg-ubuntu-12.04-server-amd64/postinstall.sh
+++ definitions/ctg-ubuntu-12.04-server-amd64/postinstall.sh
@@ -10,51 +10,32 @@ apt-get -y install linux-headers-$(uname -r) build-essential
 apt-get -y install zlib1g-dev libssl-dev libreadline-gplv2-dev
 apt-get clean
 
-# Setup sudo to allow no-password sudo for "admin"
+# Setup sudo to allow no-password sudo for "admin".
 cp /etc/sudoers /etc/sudoers.orig
 sed -i -e '/Defaults\s\+env_reset/a Defaults\texempt_group=admin' /etc/sudoers
 sed -i -e 's/%admin ALL=(ALL) ALL/%admin ALL=NOPASSWD:ALL/g' /etc/sudoers
 
-# Install NFS client
+# Install NFS client.
 apt-get -y install nfs-common
 
-# Install Ruby from source in /opt so that users of Vagrant
-# can install their own Rubies using packages or however.
-wget http://ftp.ruby-lang.org/pub/ruby/1.9/ruby-1.9.2-p290.tar.gz
-tar xvzf ruby-1.9.2-p290.tar.gz
-cd ruby-1.9.2-p290
-./configure --prefix=/opt/ruby
-make
-make install
-cd ..
-rm -rf ruby-1.9.2-p290
+# Install Ruby.
+apt-get -y install ruby rubygems
 
-# Install RubyGems 1.7.2
-wget http://production.cf.rubygems.org/rubygems/rubygems-1.8.11.tgz
-tar xzf rubygems-1.8.11.tgz
-cd rubygems-1.8.11
-/opt/ruby/bin/ruby setup.rb
-cd ..
-rm -rf rubygems-1.8.11
+# Install Puppet.
+gem install puppet --no-ri --no-rdoc
 
-# Installing chef & Puppet
-/opt/ruby/bin/gem install chef --no-ri --no-rdoc
-/opt/ruby/bin/gem install puppet --no-ri --no-rdoc
-
-# Add /opt/ruby/bin to the global path as the last resort so
-# Ruby, RubyGems, and Chef/Puppet are visible
-echo 'PATH=$PATH:/opt/ruby/bin/'> /etc/profile.d/vagrantruby.sh
-
-# Installing vagrant keys
+# Install vagrant keys.
 mkdir /home/vagrant/.ssh
 chmod 700 /home/vagrant/.ssh
 cd /home/vagrant/.ssh
+
 wget --no-check-certificate 'https://raw.github.com/mitchellh/vagrant/master/keys/vagrant.pub' -O authorized_keys
 chmod 600 /home/vagrant/.ssh/authorized_keys
 chown -R vagrant /home/vagrant/.ssh
 
 # Installing the virtualbox guest additions
 VBOX_VERSION=$(cat /home/vagrant/.vbox_version)
+
 cd /tmp
 wget http://download.virtualbox.org/virtualbox/$VBOX_VERSION/VBoxGuestAdditions_$VBOX_VERSION.iso
 mount -o loop VBoxGuestAdditions_$VBOX_VERSION.iso /mnt
